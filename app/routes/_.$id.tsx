d3b82c110eb4c11a9c57ba3921958ee7a547af10
Thant Ko Zaw <thantmkou10@gmail.com>
2024-09-10 16:55:00 +0630

    testing kv

--- app/routes/_.$id.tsx
+++ app/routes/_.$id.tsx
@@ -1,17 +1,41 @@
 import { LoaderFunctionArgs } from "@remix-run/cloudflare"
-import { Tldraw } from "tldraw"
+import { useLoaderData } from "@remix-run/react"
+import { useState } from "react"
+import { createTLStore, loadSnapshot, Tldraw } from "tldraw"
 import "tldraw/tldraw.css"
 
-export const loader = async ({ context }: LoaderFunctionArgs) => {
-  const objects = await context.cloudflare.env.CONTENT_BUCKET.list()
-  console.log(objects)
-  return null
+export const loader = async ({ context, params }: LoaderFunctionArgs) => {
+  const id = params.id ?? ""
+  if (!id) {
+    return new Response("Id is missing", { status: 400 })
+  }
+  const env = context.cloudflare.env
+  const kv = await env.TLDRAW.get(id)
+  if (!kv) {
+    return new Response("File not found", { status: 404 })
+  }
+  const object = await env.CONTENT_BUCKET.get(id)
+  if (!object) {
+    return new Response("File not found", { status: 404 })
+  }
+  const raw = await object.text()
+  return { kv, raw }
 }
 
 export default function TldrawPage() {
+  //@ts-expect-error expect err
+  const { raw } = useLoaderData<typeof loader>()
+  const [store] = useState(() => {
+    const newStore = createTLStore()
+    if (raw !== "") {
+      loadSnapshot(newStore, JSON.parse(raw))
+    }
+    return newStore
+  })
+  console.log({ raw })
   return (
     <div className="fixed inset-0">
-      <Tldraw />
+      <Tldraw store={store} />
     </div>
   )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
-import type { MetaFunction } from "@remix-run/cloudflare"
-import { Link } from "@remix-run/react"
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/cloudflare"
+import { Form, Link } from "@remix-run/react"
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,13 +11,52 @@ export const meta: MetaFunction = () => {
   ]
 }
 
+export const action = async ({ request, context }: ActionFunctionArgs) => {
+  const formData = await request.formData()
+  const name = formData.get("name")
+  if (!name) {
+    return new Response("Name is missing", { status: 400 })
+  }
+  const env = context.cloudflare.env
+
+  const id = Date.now().toString()
+  const filename = Date.now().toString() + ".json"
+  const key = `/content/${filename}`
+
+  // create a new file in r2
+  const object = await env.CONTENT_BUCKET.put(key, "")
+  if (!object) {
+    return new Response("Failed to create file", { status: 500 })
+  }
+
+  // store the file metadata in kv
+  await env.TLDRAW.put(
+    id,
+    JSON.stringify({
+      id,
+      name,
+      key,
+      filename
+    })
+  )
+}
+
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <div>
+      <div className="max-w-lg mx-auto">
         <Link to="/_" className="text-blue-600 hover:underline">
           Go to tldraw
         </Link>
+        <Form method="post">
+          <input name="name" type="text" placeholder="Enter name" />
+          <button
+            className="bg-blue-600 text-gray-100 px-2 py-1 rounded"
+            type="submit"
+          >
+            Submit
+          </button>
+        </Form>
       </div>
     </div>
   )
